3dcd6caf7ca5a0ef4ae89a9fe6ab81d781b903a2
Ayush Mishra <amishra8205@gmail.com>
2026-08-31 11:51:58 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> hash;
+        for(int i = 0; i<nums.size(); i++){
+            hash[nums[i]] += 1;
+            if(hash[nums[i]] > 1){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+};
